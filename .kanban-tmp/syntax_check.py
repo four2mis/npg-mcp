1300e72ba05fc2b7b4d6974325eb11e35880b07b
four2mis <four2mis@users.noreply.github.com>
2026-08-24 16:32:03 +0900

    chore: remove kanban scratch scripts, ignore .kanban-tmp/ [pipeline]

--- .kanban-tmp/syntax_check.py
+++
@@ -1,9 +0,0 @@
-import ast
-
-with open('npg_mcp/client.py') as f:
-    ast.parse(f.read())
-print('client.py syntax OK')
-
-with open('tests/test_client.py') as f:
-    ast.parse(f.read())
-print('test_client.py syntax OK')
